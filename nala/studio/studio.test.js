56b227aeaa0390330a95317f2fcb809ae791d6e2
cod23684 <cod23684@adobe.com>
2024-12-13 15:09:27 +0100

    debug failure

--- nala/studio/studio.test.js
+++ nala/studio/studio.test.js
@@ -2,8 +2,10 @@ import { expect, test } from '@playwright/test';
 import studioSpec from './studio.spec.js';
 import StudioPage from './studio.page.js';
 import ims from '../libs/imslogin.js';
+import take from '../libs/screenshot/take.js'
 
 const miloLibs = process.env.MILO_LIBS || '';
+const folderPath = 'screenshots/studio';
 
 let studio;
 const { features } = studioSpec;
@@ -22,6 +24,8 @@ test.describe('M@S Studio feature test suite', () => {
         page,
         baseURL,
     }) => {
+      const name = `${features[0].name}`;
+
         test.slow();
         const { data } = features[0];
         const testPage = `${baseURL}${features[0].path}${miloLibs}${features[0].browserParams}${data.cardid}`;
@@ -29,6 +33,14 @@ test.describe('M@S Studio feature test suite', () => {
 
         await test.step('step-1: Log in to MAS studio', async () => {
             await page.goto(testPage);
+            page.waitForTimeout(5000);
+            const result = await take.take(
+              page,
+              folderPath,
+              name,
+            );
+            // writeResultsToFile(folderPath, testInfo, result);
+
             await page.waitForURL(
                 '**/auth.services.adobe.com/en_US/index.html**/',
             );
