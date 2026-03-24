5d5a4465acd1e37afc6697d9923b6c1c2079df64
Blaine Gunn <Blainegunn@gmail.com>
2026-03-24 14:22:11 -0600

    MWPW-190425 New CTA Label in OST (#699)

--- studio/src/constants.js
+++ studio/src/constants.js
@@ -19,6 +19,7 @@ export const CHECKOUT_CTA_TEXTS = {
     'seven-day-trial': 'Start 7-day free trial',
     'fourteen-day-trial': 'Start 14-day free trial',
     'thirty-day-trial': 'Start 30-day free trial',
+    'six-month-free': 'Get 6-months Premium free',
     'save-today': 'Save today',
 };
 export const WCS_LANDSCAPE_PUBLISHED = 'PUBLISHED';
@@ -46,6 +47,7 @@ export const ANALYTICS_LINK_IDS = [
     'seven-day-trial',
     'fourteen-day-trial',
     'thirty-day-trial',
+    'six-month-free',
     'save-today',
 ];
 
